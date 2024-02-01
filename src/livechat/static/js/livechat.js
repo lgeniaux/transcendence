4591b0a4aa57309d3dc19dc32c8228281e6788b3
vimercie <vimercie@student.42lyon.fr>
2024-02-01 18:19:56 +0100

    Cleaned chatbox implementation

--- src/livechat/static/js/livechat.js
+++ src/livechat/static/js/livechat.js
@@ -1,1 +1,27 @@
-//get the message from the input field
+var chatForm = document.getElementById("chatForm");
+
+chatForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event)
+{
+	event.preventDefault();
+
+	var userInput = document.getElementById("userInput").value.trim();
+
+    if (userInput !== "")
+    {
+        handleFormSubmission(userInput);
+        document.getElementById("userInput").value = "";
+    }
+}
+
+function handleFormSubmission(message)
+{
+    var messageContainer = document.getElementById("messageContainer");
+    var newMessage = document.createElement("div");
+
+    newMessage.textContent = message;
+
+    messageContainer.appendChild(newMessage);
+	messageContainer.scrollTop = messageContainer.scrollHeight;
+}
