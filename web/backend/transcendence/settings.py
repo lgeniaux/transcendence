0547a51526df7d6e23004fe12af34166ba4f1fee
error7404 <48163201+error7404@users.noreply.github.com>
2024-03-29 17:47:43 +0100

    mdr

--- web/backend/transcendence/settings.py
+++ web/backend/transcendence/settings.py
@@ -16,7 +16,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -155,7 +154,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
 
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, 'frontend', 'static'),
 ]
 
 # Default primary key field type
