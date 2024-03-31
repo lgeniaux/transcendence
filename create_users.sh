c36be8cfe5b07bc6716154936736a9d6c8a1e41b
error7404 <48163201+error7404@users.noreply.github.com>
2024-03-31 22:07:06 +0200

    better tests

--- create_users.sh
+++ create_users.sh
@@ -24,10 +24,9 @@ for i in $(seq 1 $NUM_USERS); do
         -H "Content-Type: application/json" \
         -d "{\"username\": \"${USERNAME}\", \"email\": \"${EMAIL}\", \"password\": \"${PASSWORD}\"}")
     
-    if [[ "$RESPONSE" == "200" ]] || [[ "$RESPONSE" == "201" ]]; then
-        echo "User $USERNAME created successfully."
-        echo "Email: $EMAIL"
-    else
-        echo "Failed to create user $USERNAME. HTTP status: $RESPONSE"
+    printf "\033[2m\033[90mUser $USERNAME created successfully.\n"
+    printf "Email: \033[0m$EMAIL\033[2m\033[90m, Password: \033[0m$PASSWORD\n"
+    if [[ "$RESPONSE" != "200" ]] && [[ "$RESPONSE" != "201" ]]; then
+        printf "\033[2m\033[90mFailed to create user $USERNAME. HTTP status: \033[0m\033[1m\033[91m$RESPONSE\033[0m\n"
     fi
 done
